d1f25d9bd40041a135a9ffef2fcf52c7fffe38a0
Micha Ebert-Hanke <37268669+Ebert-Hanke@users.noreply.github.com>
2022-02-15 18:46:53 +0100

    Feature/refactoring (#35) (#36)
    * configuration header updated
    
    * new data files added
    
    * refactored codebase

--- src/scraping.rs
+++ src/scraping.rs
@@ -1,7 +1,10 @@
-use crate::configcreation::{BiquadParameters, PeakingWidth};
+use anyhow::{anyhow, Result};
 use scraper::{Html, Selector};
 use serde::Serialize;
 use std::collections::HashMap;
+
+use crate::configcreation::{BiquadParameters, PeakingWidth};
+use crate::Config;
 
 #[derive(Debug, Serialize)]
 pub struct CorrectionFilterSet {
@@ -17,67 +20,66 @@ impl CorrectionFilterSet {
     }
 }
 
-pub async fn get_correction_result_list(
-    client: &reqwest::Client,
-    url: &str,
-) -> Result<HashMap<String, String>, reqwest::Error> {
-    let mut link_list: HashMap<String, String> = HashMap::new();
-    let ul_selector = Selector::parse("ul").unwrap();
-    let li_selector = Selector::parse("li").unwrap();
-    let a_selector = Selector::parse("a").unwrap();
-    let raw_result = client.get(url).send().await?.text().await?;
-    let document = Html::parse_document(&raw_result);
-    for ul in document.select(&ul_selector) {
-        for li in ul.select(&li_selector) {
-            for a in li.select(&a_selector) {
-                let link_text = a.inner_html().to_lowercase();
-                let link_url = match a.value().attr("href") {
-                    Some(url) => url.to_string(),
-                    _ => "Nor Url found.".to_string(),
-                };
-                if !link_text.len() > 100 && !link_text.contains('<') && !link_text.contains('>') {
-                    link_list.insert(link_text, link_url);
-                }
-            }
-        }
-    }
-
-    Ok(link_list)
-}
-
-pub async fn collect_datafile_links(
-    client: &reqwest::Client,
-    url: &str,
-) -> Result<HashMap<String, String>, reqwest::Error> {
-    let mut link_list: HashMap<String, String> = HashMap::new();
-    let a_selector = Selector::parse("a").unwrap();
-    let raw_result = client.get(url).send().await?.text().await?;
-    let document = Html::parse_document(&raw_result);
-    for a in document.select(&a_selector) {
-        let link_text = a.inner_html().to_lowercase();
-        let link_url = match a.value().attr("href") {
-            Some(url) => url.to_string(),
-            _ => "Nor Url found.".to_string(),
-        };
-        if !link_text.len() > 100 && !link_text.contains('<') && !link_text.contains('>') {
-            link_list.insert(link_text, link_url);
-        }
-    }
-
-    Ok(link_list)
-}
-
+#[derive(Debug)]
 pub enum QueryResult {
-    Success((String, String)),
+    Success(Link),
     Suggestions(Vec<String>),
     NotFound,
+}
+
+#[derive(Debug)]
+pub struct Link {
+    pub name: String,
+    pub url: String,
+}
+impl Link {
+    pub fn new(name: String, url: String) -> Self {
+        Link { name, url }
+    }
+}
+
+pub async fn scrape_links(client: &reqwest::Client, url: &str) -> Result<HashMap<String, String>> {
+    let html = get_html(client, url).await?;
+    let links = filter_links(html);
+    Ok(links)
+}
+
+async fn get_html(client: &reqwest::Client, url: &str) -> Result<Html> {
+    let raw_result = client.get(url).send().await?.text().await?;
+    let html = Html::parse_document(&raw_result);
+    Ok(html)
+}
+
+fn filter_links(html: Html) -> HashMap<String, String> {
+    let mut link_list: HashMap<String, String> = HashMap::new();
+    let select_a = Selector::parse("a").unwrap();
+
+    for link in html.select(&select_a) {
+        if let Some(url) = link.value().attr("href") {
+            let link_text = link
+                .inner_html()
+                .to_lowercase()
+                .trim()
+                .replace("&amp;", "&")
+                .to_string();
+            let link_url = url.trim().to_string();
+            if !link_text.len() > 100
+                && !link_text.contains('<')
+                && !link_text.contains('>')
+                && link_url != "#"
+            {
+                link_list.insert(link_text, link_url);
+            }
+        };
+    }
+    link_list
 }
 
 pub fn filter_link_list(link_list: &HashMap<String, String>, query: &str) -> QueryResult {
     match link_list.get(&query.to_lowercase()) {
         Some(url) => {
             println!("Great! The {} could be found in AutoEq.", query);
-            QueryResult::Success((query.to_string(), url.to_string()))
+            QueryResult::Success(Link::new(query.to_string(), url.to_string()))
         }
         None => {
             let mut suggestions: Vec<String> = link_list.keys().cloned().collect();
@@ -94,14 +96,57 @@ pub fn filter_link_list(link_list: &HashMap<String, String>, query: &str) -> Que
     }
 }
 
-pub fn pick_url(link_list: HashMap<String, String>, query: &str) -> Option<(String, String)> {
+pub async fn scrape_eq_settings(
+    link_list: HashMap<String, String>,
+    client: &reqwest::Client,
+    config: &Config,
+) -> Result<CorrectionFilterSet> {
+    let eq_link = pick_url(link_list, &config.parametric_eq_query);
+    match eq_link {
+        Some(link) => {
+            let eq_file = client
+                .get(config.raw_eq_url(&link.url))
+                .send()
+                .await?
+                .text()
+                .await?;
+            let mut data = eq_file.lines();
+            let preamp_gain = parse_preamp_gain(&mut data)?;
+            let mut filterset = CorrectionFilterSet::new(preamp_gain);
+            parse_filters(&mut data, &mut filterset)?;
+            Ok(filterset)
+        }
+        None => Err(anyhow!("The eq data could not be parsed.")),
+    }
+}
+
+fn pick_url(link_list: HashMap<String, String>, query: &str) -> Option<Link> {
     link_list
         .into_iter()
         .find(|(k, _v)| k.to_lowercase().contains(&query.to_lowercase()))
-        .map(|(k, v)| (k, v))
+        .map(|(k, v)| Link::new(k, v))
 }
 
-pub fn parse_filter_line(line: &str) -> Result<BiquadParameters, Box<dyn std::error::Error>> {
+fn parse_preamp_gain(lines: &mut std::str::Lines) -> Result<f32> {
+    let gain = lines
+        .next()
+        .ok_or(anyhow!("Not enough lines."))?
+        .split(' ')
+        .nth(1)
+        .ok_or(anyhow!("Not enough elements."))?
+        .parse::<f32>()?;
+    Ok(gain)
+}
+
+fn parse_filters(lines: &mut std::str::Lines, filterset: &mut CorrectionFilterSet) -> Result<()> {
+    for line in lines.skip(0) {
+        let eq = parse_filter_line(line)?;
+        filterset.eq_bands.push(eq);
+    }
+    Ok(())
+}
+
+fn parse_filter_line(line: &str) -> Result<BiquadParameters> {
     let mut split_line = line.split(' ');
     let fc = split_line.nth(5);
     let gain = split_line.nth(2);
