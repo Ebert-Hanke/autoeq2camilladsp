8b02fd6eeb179a89639303fe8600da6817e1f9d4
Micha Ebert-Hanke <mail@ebert-hanke.de>
2022-06-11 20:28:23 +0200

    basic JSON output for headphone list and preset options

--- src/noninteractive.rs
+++ src/noninteractive.rs
@@ -1,6 +1,8 @@
 use anyhow::Result;
 use clap::{Parser, Subcommand};
+use serde::Serialize;
 
+use crate::configcreation::Crossfeed;
 use crate::scraping::scrape_links;
 use crate::{CliMode, Config};
 
@@ -18,17 +20,30 @@ enum Commands {
     Init,
 }
 
-pub async fn noninteractive_mode(client: &reqwest::Client, config: &Config) -> Result<()> {
-    let cli = Cli::parse();
-    if let Some(input) = cli.command {
-        match input {
-            Commands::Init => {
-                let database_result_list = scrape_links(client, &config.repo_url()).await?;
-                println!("{:?}", database_result_list);
-            }
+#[derive(Serialize)]
+struct OutputJson {
+    #[serde(rename(serialize = "autoeqList"))]
+    autoeq_list: Vec<Headphone>,
+    crossfeed: Vec<String>,
+}
+impl OutputJson {
+    fn new() -> OutputJson {
+        OutputJson {
+            autoeq_list: Vec::new(),
+            crossfeed: vec![
+                Crossfeed::None.to_string(),
+                Crossfeed::PowChuMoy.to_string(),
+                Crossfeed::Mpm.to_string(),
+                Crossfeed::Natural.to_string(),
+            ],
         }
     }
-    Ok(())
+}
+
+#[derive(Serialize)]
+struct Headphone {
+    name: String,
+    link: String,
 }
 
 pub fn cli_mode_check() -> CliMode {
@@ -37,4 +52,35 @@ pub fn cli_mode_check() -> CliMode {
         Some(_input) => CliMode::NonInteractive,
         None => CliMode::Interactive,
     }
+}
+
+pub async fn noninteractive_mode(client: &reqwest::Client, config: &Config) -> Result<()> {
+    let cli = Cli::parse();
+    if let Some(input) = cli.command {
+        match input {
+            Commands::Init => {
+                create_json(client, config).await?;
+            }
+        }
+    }
+    Ok(())
+}
+
+async fn create_json(client: &reqwest::Client, config: &Config) -> Result<()> {
+    let database_result_list = scrape_links(client, &config.repo_url()).await?;
+
+    let mut json = OutputJson::new();
+
+    for (key, val) in database_result_list.iter() {
+        json.autoeq_list.push(Headphone {
+            name: key.to_owned(),
+            link: val.to_owned(),
+        });
+    }
+
+    let json_out = serde_json::to_string(&json)?;
+
+    println!("{}", json_out);
+
+    Ok(())
 }
